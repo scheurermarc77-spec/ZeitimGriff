449981b7ef944a1f28c0fbba603b54efcfb7e331
scheurermarc77-spec <scheurermarc77@gmail.com>
2026-08-16 18:26:40 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+ZeitimGriff – Firebase-Version
+Diese Version ist bereits mit deinem Firebase-Projekt verbunden.
+Upload: kompletten Ordnerinhalt auf GitHub Pages/Webspace laden.
+Danach auf dem iPhone in Safari öffnen > Teilen > Zum Home-Bildschirm.
+Wichtig: Firestore läuft aktuell im Testmodus. Nach dem Funktionstest die Regeln absichern.
---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.TIMEAPP_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAxBYrLYiSXT9av7Qkxpyijiik0bUZqWTA",
+  authDomain: "zeitimgriff-12634.firebaseapp.com",
+  projectId: "zeitimgriff-12634",
+  storageBucket: "zeitimgriff-12634.firebasestorage.app",
+  messagingSenderId: "617831467718",
+  appId: "1:617831467718:web:32b6e3aec9a56f3133a5c7"
+};
